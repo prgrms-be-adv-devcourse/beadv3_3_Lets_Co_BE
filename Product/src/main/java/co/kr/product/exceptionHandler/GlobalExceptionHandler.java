42145e1ea13cc0a0f6615c29a5ec1f97f1cb2a17
Lujol <148629234+Lujol@users.noreply.github.com>
2026-01-19 01:49:18 +0900

    Feat: add ExceptionHandler
    - add MethodArgumentNotValidException handler

---
+++ Product/src/main/java/co/kr/product/exceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package co.kr.product.exceptionHandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // @Valid 추가 후, 해당 오류 표시를 위한 핸들러
+    // 추가 공부 필요
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+}

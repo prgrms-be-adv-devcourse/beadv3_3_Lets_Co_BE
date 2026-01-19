38a7d3e51ee34a7399a6913d7dd4e49cfb447998
CHOI <gnb1202@naver.com>
2026-01-19 10:30:25 +0900

    Fix/SettlementTyp-chk-enum
    db chk 반영하여 SettlementType 수정
    
    수정
    - SettlementType

--- Order/src/main/java/co/kr/order/model/vo/SettlementType.java
+++ Order/src/main/java/co/kr/order/model/vo/SettlementType.java
@@ -1,12 +1,12 @@
 package co.kr.order.model.vo;
 
 /**
- * 정산 유형
+ * 정산 상태 VO
  * - SALE: 판매 정산 (주문 완료 시 발생)
  * - REFUND: 환불 차감 (환불 시 발생)
  */
 public enum SettlementType {
-    SALE,
-    REFUND,
-    COMPLETED
+    Orders_CONFIRMED,
+    SETTLE_PAYOUT,
+    CANCEL_ADJUST
 }

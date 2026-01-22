837cd28caa9e95dafa292836d3230161589f2a82
Lujol <148629234+Lujol@users.noreply.github.com>
2026-01-23 06:18:50 +0900

    Fix: bug

--- Product/src/main/java/co/kr/product/product/dto/vo/ProductStatus.java
+++ Product/src/main/java/co/kr/product/product/dto/vo/ProductStatus.java
@@ -4,5 +4,7 @@ public enum ProductStatus {
     ON_SALE,    // 보임 (판매 중)
     SOLD_OUT,   // 품절
     STOPPED,    // 숨김 (판매 x - 판매자)
-    BLOCKED     // 블락 처리(판매 중지 - 관리자)
+    BLOCKED,
+    STOP_SELLING, 
+    HIDE;// 블락 처리(판매 중지 - 관리자)
 }

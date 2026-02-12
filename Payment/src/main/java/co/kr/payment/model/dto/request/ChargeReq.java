69907fd75eaceacb9795cbfa4933a0bf83c7f872
JammySeok <jjh326326@naver.com>
2026-02-12 22:27:14 +0900

    Fix/payment-charge-bugFix_jus
    결제 서비스 예치금 충전 버그 픽스
    
    발생 이유:
    충전 엔드포인트를 /client/payments/charge로 하면
    유저가(프론트엔드 에서) 충전 요청을 할 수 없음
    
    해결:
    엔드포인트를 /payments/charge 로 바꿔주고, 이에 맞게 로직을 수정함
    
    + 결제 중 Order와의 FeignClient 통신 중 다시 Order를 부르면서 순환 의존 구조가 발생 (주문 상태 변경)
    -> Order에게 보내는 FeignClient 코드를 제거
    
    수정
    - UserClient
    - PaymentApiController
    - BalanceController
    - ChargeReq
    - PaymentService/Impl

--- Payment/src/main/java/co/kr/payment/model/dto/request/ChargeReq.java
+++ Payment/src/main/java/co/kr/payment/model/dto/request/ChargeReq.java
@@ -8,9 +8,6 @@ import java.math.BigDecimal;
 
 public record ChargeReq(
 
-        @NotNull(message = "유저 인덱스는 필수입니다.")
-        Long userIdx,
-
         @NotNull(message = "충전 금액은 필수입니다.")
         @Positive(message = "충전 금액은 0보다 커야 합니다.")
         BigDecimal amount,

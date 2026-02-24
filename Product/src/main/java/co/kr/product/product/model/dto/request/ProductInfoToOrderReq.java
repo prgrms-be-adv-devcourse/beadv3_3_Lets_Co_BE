73c0b2a9bdb5e629bc5bf476997652a25c174ac0
Lujol <148629234+Lujol@users.noreply.github.com>
2026-02-24 10:18:58 +0900

    FIx : bug
    - products -> product

--- Product/src/main/java/co/kr/product/product/model/dto/request/ProductInfoToOrderReq.java
+++ Product/src/main/java/co/kr/product/product/model/dto/request/ProductInfoToOrderReq.java
@@ -7,7 +7,7 @@ public record ProductInfoToOrderReq(
 
         @NotNull(message = "상품 코드는 필수입니다.")
         @Positive(message = "유효하지 않은 상품 ID입니다.")
-        String productsCode,
+        String productCode,
 
         @NotNull(message = "옵션 코드는 필수입니다.")
         @Positive(message = "유효하지 않은 옵션 ID입니다.")

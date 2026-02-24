c6371c06c050b1525d501f6a1215ede6a0369e78
Lujol <148629234+Lujol@users.noreply.github.com>
2026-02-24 14:08:39 +0900

    Fix: bug
    - bulk requst 수정

--- Product/src/main/java/co/kr/product/product/model/dto/request/ProductInfoToOrderReq.java
+++ Product/src/main/java/co/kr/product/product/model/dto/request/ProductInfoToOrderReq.java
@@ -6,11 +6,9 @@ import jakarta.validation.constraints.Positive;
 public record ProductInfoToOrderReq(
 
         @NotNull(message = "상품 코드는 필수입니다.")
-        @Positive(message = "유효하지 않은 상품 ID입니다.")
         String productCode,
 
         @NotNull(message = "옵션 코드는 필수입니다.")
-        @Positive(message = "유효하지 않은 옵션 ID입니다.")
         String optionCode
 ) {
 
